e724c4f68af1835e7f81dc91c5af13547d71fccc
OwaisNaseer <owais366543@gmail.com>
2024-08-07 19:04:48 +0500

    add custom group checkboxes

--- src/components/shared/CustomCheckbox.jsx
+++ src/components/shared/CustomCheckbox.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import { CheckboxIcon } from "../../assets/icons";
 
-export default function CustomCheckbox({ onChange, checked, title, details }) {
+export default function CustomCheckbox({ onChange, checked, title, details, id, index }) {
   return (
     <div className="flex items-center">
       <input
         type="checkbox"
-        id="checkbox"
+        checked={checked}
+        id={id}
         className="peer hidden"
-        onChange={onChange}
+        onChange={() => onChange(index)}
       />
       <label
-        htmlFor="checkbox"
-        className="w-[600px] h-[90px] rounded-[5px]  border-2 flex justify-between items-center px-5 cursor-pointer select-none peer-checked:border-blue-700 peer-checked:border-l-8 peer-checked:pl-[14px] "
+        htmlFor={id}
+        className="w-[600px] h-auto rounded-[5px] border-2 flex justify-between items-center px-5 py-5 cursor-pointer select-none peer-checked:border-blue-700 peer-checked:border-l-8 peer-checked:pl-[14px]"
       >
-        <div>
+        <div className="w-[90%]">
           <h3 className="font-bold">{title}</h3>
-          <span>{details}</span>
+          <span className="text-wrap">{details}</span>
         </div>
         <div
-          className={
-            "border-2 h-[25px] w-[25px] flex justify-center items-center rounded-[5px]"
-          }
+          className="border-2 h-[25px] w-[25px] flex justify-center items-center rounded-[5px]"
         >
           <div className={`peer-checked:block ${checked ? "block" : "hidden"}`}>
             <CheckboxIcon />
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,22 +1,70 @@
 import React from "react";
 import CustomCheckbox from "../../components/shared/CustomCheckbox";
-export default function Home() {
-  const [checked, setChecked] = React.useState(false);
 
-  function handleChange(e) {
-    setChecked(e.target.checked);
+export default function Home() {
+  const data = [
+    {
+      id: 1,
+      title: "Lake house",
+      details: "6661 spring street, Columbus 48228-4763",
+    },
+    {
+      id: 2,
+      title: "Home",
+      details: "6661 spring street, Columbus 48228-4763",
+    },
+    {
+      id: 3,
+      title: "Office",
+      details: "6661 spring street, Columbus 48228-4763",
+    },
+    {
+      id: 4,
+      title: "Shop",
+      details: "6661 spring street, Columbus 48228-4763",
+    },
+    {
+      id: 5,
+      title: "work place",
+      details: "6661 spring street, Columbus 48228-4763",
+    },
+  ];
+
+  const [checked, setChecked] = React.useState(
+    new Array(data.length).fill(false)
+  );
+
+  const [selected, setselected] = React.useState([]);
+  function handleChange(index) {
+    const updatedChecked = checked.map((item, idx) =>
+      idx === index ? !item : item
+    );
+    setChecked(updatedChecked);
+
+    const newselected = [...selected];
+    if (updatedChecked[index]) {
+      newselected.push(data[index].title);
+    } else {
+      const titleIndex = newselected.indexOf(data[index].title);
+      newselected.splice(titleIndex, 1);
+    }
+    setselected(newselected);
   }
+
   return (
     <div className="flex flex-col w-screen h-screen justify-center items-center gap-1">
-      <div className="flex flex-col items-start">
-        <CustomCheckbox
-          onChange={handleChange}
-          checked={checked}
-          setChecked={setChecked}
-          title={"Lake house"}
-          details={"6661 spring street, Columbus 48228-4763"}
-        />
-      </div>
+      {data.map((item, index) => (
+        <div key={item.id} className="flex flex-col items-start">
+          <CustomCheckbox
+            onChange={() => handleChange(index)}
+            checked={checked[index]}
+            title={item.title}
+            details={item.details}
+            id={item.id}
+            index={index}
+          />
+        </div>
+      ))}
     </div>
   );
 }
